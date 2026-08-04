0436d8d63a43183cff4a83d9010f1acd14466288
web-devs <mohammedshameem636@gmail.com>
2026-08-05 00:34:36 +0530

    setup backend skeleton with express + mongoDB atlas

---
+++ server/src/server.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const connectDB = require('./config/db');
+
+const app = express();
+connectDB();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.get('/ping', (req, res) => res.json({ status: 'ok' }));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
