2691c77e13110a9fd3312b4000b7780412e44035
ReshmaSulthana023 <reshmasulthana134@gmal.com>
2026-08-22 00:06:08 +0530

    updated

--- server/src/models/Appointment.js
+++ server/src/models/Appointment.js
@@ -10,7 +10,7 @@ const appointmentSchema = new mongoose.Schema(
 
     doctor: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      ref: "Patient",
       required: true,
     },
 
